c75bf2c5d8df5196f3226564cd9fc2f88a329df0
Matthew Heun <matthew.heun@me.com>
2023-04-27 11:55:52 -0400

    Working on pipeline.

--- _targets_r/targets/ruvy_df.R
+++ _targets_r/targets/ruvy_df.R
@@ -1,9 +1,16 @@
 tar_target(ruvy_df, {
   iron_steel |> 
-    dplyr::group_by(Year, Country, Matrix) |> 
+    dplyr::group_by(Year, Country, Energy.type, Matrix) |> 
     matsindf::collapse_to_matrices(matnames = "Matrix", 
                                    rownames = "From", 
                                    colnames = "To",
                                    matvals = "Value") |> 
-    tidyr::pivot_wider(names_from = "Matrix", values_from = "Value")
+    tidyr::pivot_wider(names_from = "Matrix", values_from = "Value") |> 
+    # Set row and column types
+    dplyr::mutate(
+      R = R |> matsbyname::setrowtype("Industry") |> matsbyname::setcoltype("Product"), 
+      U = U |> matsbyname::setrowtype("Product") |> matsbyname::setrowtype("Industry"), 
+      V = V |> matsbyname::setrowtype("Industry") |> matsbyname::setcoltype("Product"), 
+      Y = Y |> matsbyname::setrowtype("Product") |> matsbyname::setrowtype("Industry")
+    )
 })
