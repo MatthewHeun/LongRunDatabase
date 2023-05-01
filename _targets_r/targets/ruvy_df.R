ba15631a27ea949d501c0290df640ac3ac4a58a1
Matthew Heun <matthew.heun@me.com>
2023-05-01 13:44:02 -0400

    Added efficiency calculations.

--- _targets_r/targets/ruvy_df.R
+++ _targets_r/targets/ruvy_df.R
@@ -7,11 +7,13 @@ tar_target(ruvy_df, {
                                    matvals = "Value",
                                    matrix.class = "matrix") |> 
     tidyr::pivot_wider(names_from = "Matrix", values_from = "Value") |> 
-    # Set row and column types
     dplyr::mutate(
+      # Set row and column types
       R = R |> matsbyname::setrowtype("Industry") |> matsbyname::setcoltype("Product"), 
       U = U |> matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry"), 
       V = V |> matsbyname::setrowtype("Industry") |> matsbyname::setcoltype("Product"), 
       Y = Y |> matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry")
-    )
+    ) |> 
+    # Join with the S_units matrices
+    dplyr::left_join(s_units)
 })
