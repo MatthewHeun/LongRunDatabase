b53947d7a2d81b781bbac9ebef1d1f87395a4b01
Matthew Heun <matthew.heun@me.com>
2023-06-27 12:52:26 -0400

    Updated for Ricardo's latest data.

--- _targets_r/targets/ruvy_df.R
+++ _targets_r/targets/ruvy_df.R
@@ -5,15 +5,22 @@ tar_target(ruvy_df, {
                                    rownames = "From", 
                                    colnames = "To",
                                    matvals = "Value",
-                                   matrix.class = "matrix") |> 
+                                   matrix_class = "matrix") |> 
     tidyr::pivot_wider(names_from = "Matrix", values_from = "Value") |> 
     dplyr::mutate(
       # Set row and column types
       R = R |> matsbyname::setrowtype("Industry") |> matsbyname::setcoltype("Product"), 
       U = U |> matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry"), 
       V = V |> matsbyname::setrowtype("Industry") |> matsbyname::setcoltype("Product"), 
-      Y = Y |> matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry")
+      Y = Y |> matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry"), 
+      # Create other expected matrices
+      # The U_feed matrix is same as U
+      U_feed = U, 
+      # Create a U_EIOU matrix full of zeros but with same rows and columns as U
+      U_EIOU = matsbyname::hadamardproduct_byname(U, 0),
+      # Create an r_EIOU matrix, again full of zeroes
+      r_EIOU = U_EIOU
     ) |> 
     # Join with the S_units matrices
-    dplyr::left_join(s_units)
+    dplyr::left_join(s_units, by = c("Year", "Country", "Energy.type"))
 })
