7e5f14aacf3162537dfcd7fa227a09b082fca34f
Matthew Heun <matthew.heun@me.com>
2023-07-02 13:26:18 -0400

    Deleting file and dirs that can be regenerated.

--- _targets_r/globals/example-globals.R
+++
@@ -1,7 +0,0 @@
-options(tidyverse.quiet = TRUE)
-tar_option_set(packages = c("dplyr", "ggplot2", "readxl", "tidyr", "matsindf", "Recca"))
-
-read_data <- function(.path, sheet) {
-  .path |> 
-    readxl::read_excel(sheet = sheet) 
-}
--- _targets_r/targets/iron_steel.R
+++
@@ -1,3 +0,0 @@
-tar_target(iron_steel, {
-  read_data("data/Iron_Steel_data_US_2011_2001.xlsx", sheet = "Iron and steel")
-})
